f2072d66e1e7e6dfeec874cc905cd87cb5d5c6d4
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2025-02-16 12:10:48 +0530

    minor changes in app ui and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="bg-gray-800">
+  <footer className="bg-gray-700">
     <div className="w-full py-12 px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div>
