abf421374d7df16c350e30e6e27c6cdd1a745558
9keystrokes <1.nayanmandal@gmail.com>
2025-03-02 16:22:32 +0530

    logs removed

--- server/middleware/isAuthenticated.js
+++ server/middleware/isAuthenticated.js
@@ -2,7 +2,7 @@
 module.exports = {
     isAuthenticated: function(req, res, next) {
       if (req.isAuthenticated()) {
-        console.log('User authenticated:', req.user); // Add logging
+        // console.log('User authenticated:', req.user); // Add logging
         return next();
       }
       return res.status(401).json({ message: 'Not authenticated' }); // Return JSON instead of redirect
